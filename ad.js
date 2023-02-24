42d2f4bfe679ec71f031bd7e8f6147b1c728f7f0
ipillonib <116582490+ipillonib@users.noreply.github.com>
2023-02-24 09:22:41 -0600

    Update ad.js
    prueba csb AD

--- ad.js
+++ ad.js
@@ -2,23 +2,23 @@ const ActiveDirectory = require('activedirectory');
 const express = require('express');
 const http = require('http');
 var config = {
-    //*
+    /*
     url: 'ldap://34.71.5.66:389',
     baseDN: 'CN=Users,DC=TECH,DC=LOCAL',
     username: 'ceadmin',
     password: 'Gapsi123.'
-    //*/
-   /*
+    /*/
+   //*
     url: 'ldap://consupago.com:389',
     baseDN: 'DC=consupago,DC=com',
     username: 'appsibm',
     password: 'Santafe01$'
-    */
+   // */
 };
 //filter: '(&(objectcategory=person)(objectclass=user)(|(samaccountname={{username}})(mail={{mail}})))'
 //filter: '(&(objectcategory=*)(|(samaccountname={{username}})))'
 
-var sAMAccountName = 'ceadmin';
+var sAMAccountName = 'appsibm';
 var ad = new ActiveDirectory(config);
 
 var username = 'appsibm';
